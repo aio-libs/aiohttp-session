6b5cc860793121a44a75df0b6a45e7f56fd3c6d0
Ihor Gorobets <ihorgorobets@gmail.com>
2015-05-30 07:21:25 +0300

    Add Fernet sipher

--- aiohttp_session/cookie_storage.py
+++ aiohttp_session/cookie_storage.py
@@ -3,9 +3,9 @@ import json
 import base64
 from . import AbstractStorage, Session
 
-from Crypto.Cipher import AES
-from Crypto import Random
+from cryptography.fernet import Fernet
 
+key =  b'klef1rMWEwD5VGJHAMfBa7vwIygLblPRuODdJLX_C2U='
 
 class EncryptedCookieStorage(AbstractStorage):
     """Encrypted JSON storage.
@@ -19,10 +19,12 @@ class EncryptedCookieStorage(AbstractStorage):
                          httponly=httponly)
 
         self._secret_key = secret_key
-        if len(self._secret_key) % AES.block_size != 0:
+
+        if len(self._secret_key) < 44:
             raise TypeError(
-                'Secret key must be a multiple of {} in length'.format(
-                    AES.block_size))
+                'Secret key must be a least {} in length'.format(
+                    44))
+        self.cipher = Fernet(key)
 
     @asyncio.coroutine
     def load_session(self, request):
@@ -31,24 +33,13 @@ class EncryptedCookieStorage(AbstractStorage):
             return Session(None, new=True)
         else:
             cookie = base64.b64decode(cookie)
-            iv = cookie[:AES.block_size]
-            data = cookie[AES.block_size:]
-            cipher = AES.new(self._secret_key, AES.MODE_CBC, iv)
-            decrypted = cipher.decrypt(data)
+            decrypted = self.cipher.decrypt(data)
             data = json.loads(decrypted.decode('utf-8'))
             return Session(None, data=data, new=False)
 
     @asyncio.coroutine
     def save_session(self, request, response, session):
         cookie_data = json.dumps(session._mapping).encode('utf-8')
-        if len(cookie_data) % AES.block_size != 0:
-            # padding with spaces to full blocks
-            to_pad = AES.block_size - (len(cookie_data) % AES.block_size)
-            cookie_data += b' ' * to_pad
-
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self._secret_key, AES.MODE_CBC, iv)
-        encrypted = cipher.encrypt(cookie_data)
-        encrypted = iv + encrypted
+        encrypted = self.cipher.encrypt(cookie_data)
         b64coded = base64.b64encode(encrypted).decode('utf-8')
         self.save_cookie(response, b64coded)

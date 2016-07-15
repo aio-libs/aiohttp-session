6cc8a941e6744364ff4ba189587efee63601fdeb
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-07-15 18:08:15 +0300

    Convert nacl test to pytest style

--- tests/test_nacl_storage.py
+++ tests/test_nacl_storage.py
@@ -1,176 +1,139 @@
 import asyncio
 import json
-import socket
-import unittest
 import time
 
+import pytest
 import nacl.secret
 import nacl.utils
-from aiohttp import web, request
+from aiohttp import web
 from nacl.encoding import Base64Encoder
 
 from aiohttp_session import Session, session_middleware, get_session
 from aiohttp_session.nacl_storage import NaClCookieStorage
 
 
-class TestNaClCookieStorage(unittest.TestCase):
+def test_invalid_key():
+    with pytest.raises(ValueError):
+        NaClCookieStorage(b'123')  # short key
 
-    def setUp(self):
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(None)
 
-        self.key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
-        self.secretbox = nacl.secret.SecretBox(self.key)
-        self.handler = None
-        self.srv = None
+def make_cookie(client, secretbox, data):
+    session_data = {
+        'session': data,
+        'created': int(time.time())
+    }
 
-    def tearDown(self):
-        if self.handler is not None:
-            self.loop.run_until_complete(self.handler.finish_connections())
-        if self.srv is not None:
-            self.srv.close()
-        self.loop.stop()
-        self.loop.run_forever()
-        self.loop.close()
+    cookie_data = json.dumps(session_data).encode('utf-8')
+    nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
+    data = secretbox.encrypt(cookie_data, nonce,
+                             encoder=Base64Encoder).decode('utf-8')
 
-    def find_unused_port(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('127.0.0.1', 0))
-        port = s.getsockname()[1]
-        s.close()
-        return port
+    client.session.cookies['AIOHTTP_SESSION'] = data
+
+
+def create_app(loop, handler, key):
+    middleware = session_middleware(NaClCookieStorage(key))
+    app = web.Application(middlewares=[middleware], loop=loop)
+    app.router.add_route('GET', '/', handler)
+    return app
+
+
+def decrypt(secretbox, cookie_value):
+    assert type(cookie_value) == str
+    return json.loads(
+        secretbox.decrypt(cookie_value.encode('utf-8'),
+                          encoder=Base64Encoder).decode('utf-8')
+    )
+
+
+@pytest.fixture
+def secretbox(key):
+    return nacl.secret.SecretBox(key)
+
+
+@pytest.fixture
+def key():
+    return nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+
+
+@asyncio.coroutine
+def test_create_new_sesssion(test_client, secretbox, key):
 
     @asyncio.coroutine
-    def create_server(self, method, path, handler):
-        middleware = session_middleware(
-            NaClCookieStorage(self.key))
-        app = web.Application(middlewares=[middleware], loop=self.loop)
-        app.router.add_route(method, path, handler)
+    def handler(request):
+        session = yield from get_session(request)
+        assert isinstance(session, Session)
+        assert session.new
+        assert not session._changed
+        assert {} == session
+        return web.Response(body=b'OK')
 
-        port = self.find_unused_port()
-        handler = app.make_handler()
-        srv = yield from self.loop.create_server(
-            handler, '127.0.0.1', port)
-        url = "http://127.0.0.1:{}".format(port) + path
-        self.srv = srv
-        self.handler = handler
-        return app, srv, url
+    client = yield from test_client(create_app, handler, key)
+    resp = yield from client.get('/')
+    assert resp.status == 200
 
-    def make_cookie(self, data):
-        session_data = {
-            'session': data,
-            'created': int(time.time())
-        }
 
-        cookie_data = json.dumps(session_data).encode('utf-8')
-        nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
-        data = self.secretbox.encrypt(cookie_data, nonce,
-                                      encoder=Base64Encoder).decode('utf-8')
+@asyncio.coroutine
+def test_load_existing_sesssion(test_client, secretbox, key):
 
-        return {'AIOHTTP_SESSION': data}
+    @asyncio.coroutine
+    def handler(request):
+        session = yield from get_session(request)
+        assert isinstance(session, Session)
+        assert not session.new
+        assert not session._changed
+        assert {'a': 1, 'b': 12} == session
+        return web.Response(body=b'OK')
 
-    def decrypt(self, cookie_value):
-        assert type(cookie_value) == str
-        return json.loads(
-            self.secretbox.decrypt(cookie_value.encode('utf-8'),
-                                   encoder=Base64Encoder).decode('utf-8')
-        )
+    client = yield from test_client(create_app, handler, key)
+    make_cookie(client, secretbox, {'a': 1, 'b': 12})
+    resp = yield from client.get('/')
+    assert resp.status == 200
 
-    def test_invalid_key(self):
-        with self.assertRaises(ValueError):
-            NaClCookieStorage(b'123')  # short key
 
-    def test_create_new_sesssion(self):
+@asyncio.coroutine
+def test_change_session(test_client, secretbox, key):
 
-        @asyncio.coroutine
-        def handler(request):
-            session = yield from get_session(request)
-            self.assertIsInstance(session, Session)
-            self.assertTrue(session.new)
-            self.assertFalse(session._changed)
-            self.assertEqual({}, session)
-            return web.Response(body=b'OK')
+    @asyncio.coroutine
+    def handler(request):
+        session = yield from get_session(request)
+        session['c'] = 3
+        return web.Response(body=b'OK')
 
-        @asyncio.coroutine
-        def go():
-            _, _, url = yield from self.create_server('GET', '/', handler)
-            resp = yield from request('GET', url, loop=self.loop)
-            self.assertEqual(200, resp.status)
+    client = yield from test_client(create_app, handler, key)
+    make_cookie(client, secretbox, {'a': 1, 'b': 2})
+    resp = yield from client.get('/')
+    assert resp.status == 200
 
-        self.loop.run_until_complete(go())
+    morsel = resp.cookies['AIOHTTP_SESSION']
+    cookie_data = decrypt(secretbox, morsel.value)
+    assert 'session' in cookie_data
+    assert 'a' in cookie_data['session']
+    assert 'b' in cookie_data['session']
+    assert 'c' in cookie_data['session']
+    assert 'created' in cookie_data
+    assert cookie_data['session']['a'] == 1
+    assert cookie_data['session']['b'] == 2
+    assert cookie_data['session']['c'] == 3
+    assert morsel['httponly']
+    assert '/' == morsel['path']
 
-    def test_load_existing_sesssion(self):
 
-        @asyncio.coroutine
-        def handler(request):
-            session = yield from get_session(request)
-            self.assertIsInstance(session, Session)
-            self.assertFalse(session.new)
-            self.assertFalse(session._changed)
-            self.assertEqual({'a': 1, 'b': 12}, session)
-            return web.Response(body=b'OK')
+@asyncio.coroutine
+def test_clear_cookie_on_session_invalidation(test_client, secretbox, key):
 
-        @asyncio.coroutine
-        def go():
-            _, _, url = yield from self.create_server('GET', '/', handler)
-            resp = yield from request(
-                'GET', url,
-                cookies=self.make_cookie({'a': 1, 'b': 12}),
-                loop=self.loop)
-            self.assertEqual(200, resp.status)
+    @asyncio.coroutine
+    def handler(request):
+        session = yield from get_session(request)
+        session.invalidate()
+        return web.Response(body=b'OK')
 
-        self.loop.run_until_complete(go())
+    client = yield from test_client(create_app, handler, key)
+    make_cookie(client, secretbox, {'a': 1, 'b': 2})
+    resp = yield from client.get('/')
+    assert resp.status == 200
 
-    def test_change_session(self):
-
-        @asyncio.coroutine
-        def handler(request):
-            session = yield from get_session(request)
-            session['c'] = 3
-            return web.Response(body=b'OK')
-
-        @asyncio.coroutine
-        def go():
-            _, _, url = yield from self.create_server('GET', '/', handler)
-            resp = yield from request(
-                'GET', url,
-                cookies=self.make_cookie({'a': 1, 'b': 2}),
-                loop=self.loop)
-            self.assertEqual(200, resp.status)
-            morsel = resp.cookies['AIOHTTP_SESSION']
-            cookie_data = self.decrypt(morsel.value)
-            self.assertIn('session', cookie_data)
-            self.assertIn('a', cookie_data['session'])
-            self.assertIn('b', cookie_data['session'])
-            self.assertIn('c', cookie_data['session'])
-            self.assertIn('created', cookie_data)
-            self.assertEqual(cookie_data['session']['a'], 1)
-            self.assertEqual(cookie_data['session']['b'], 2)
-            self.assertEqual(cookie_data['session']['c'], 3)
-            self.assertTrue(morsel['httponly'])
-            self.assertEqual('/', morsel['path'])
-
-        self.loop.run_until_complete(go())
-
-    def test_clear_cookie_on_session_invalidation(self):
-
-        @asyncio.coroutine
-        def handler(request):
-            session = yield from get_session(request)
-            session.invalidate()
-            return web.Response(body=b'OK')
-
-        @asyncio.coroutine
-        def go():
-            _, _, url = yield from self.create_server('GET', '/', handler)
-            resp = yield from request(
-                'GET', url,
-                cookies=self.make_cookie({'a': 1, 'b': 2}),
-                loop=self.loop)
-            self.assertEqual(200, resp.status)
-            morsel = resp.cookies['AIOHTTP_SESSION']
-            self.assertEqual('', morsel.value)
-            self.assertFalse(morsel['httponly'])
-            self.assertEqual(morsel['path'], '/')
-
-        self.loop.run_until_complete(go())
+    morsel = resp.cookies['AIOHTTP_SESSION']
+    assert '' == morsel.value
+    assert not morsel['httponly']
+    assert morsel['path'] == '/'

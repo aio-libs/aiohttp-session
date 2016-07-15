b45ec15245c1eef376f3b201489a18638015e4f6
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-07-15 16:23:57 +0300

    Convert fernet test to pytest style

--- tests/test_encrypted_cookie_storage.py
+++ tests/test_encrypted_cookie_storage.py
@@ -1,181 +1,145 @@
 import asyncio
 import json
-import socket
-import unittest
 import base64
 import time
 
-from aiohttp import web, request
+import pytest
+from aiohttp import web
 
-from cryptography import fernet
+from cryptography.fernet import Fernet
 
 from aiohttp_session import Session, session_middleware, get_session
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
 
 
-class TestEncryptedCookieStorage(unittest.TestCase):
+def make_cookie(client, fernet, data):
+    session_data = {
+        'session': data,
+        'created': int(time.time())
+    }
 
-    def setUp(self):
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(None)
+    cookie_data = json.dumps(session_data).encode('utf-8')
+    data = fernet.encrypt(cookie_data).decode('utf-8')
 
-        key = fernet.Fernet.generate_key()
-        self.fernet = fernet.Fernet(key)
-        self.key = base64.urlsafe_b64decode(key)
-        self.handler = None
-        self.srv = None
+    client.session.cookies['AIOHTTP_SESSION'] = data
 
-    def tearDown(self):
-        if self.handler is not None:
-            self.loop.run_until_complete(self.handler.finish_connections())
-        if self.srv is not None:
-            self.srv.close()
-        self.loop.stop()
-        self.loop.run_forever()
-        self.loop.close()
 
-    def find_unused_port(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('127.0.0.1', 0))
-        port = s.getsockname()[1]
-        s.close()
-        return port
+def create_app(loop, handler, key):
+    middleware = session_middleware(EncryptedCookieStorage(key))
+    app = web.Application(middlewares=[middleware], loop=loop)
+    app.router.add_route('GET', '/', handler)
+    return app
+
+
+def decrypt(fernet, cookie_value):
+    assert type(cookie_value) == str
+    return json.loads(
+        fernet.decrypt(cookie_value.encode('utf-8')).decode('utf-8')
+    )
+
+
+@pytest.fixture
+def fernet_and_key():
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+    return fernet, base64.urlsafe_b64decode(key)
+
+
+@pytest.fixture
+def fernet(fernet_and_key):
+    return fernet_and_key[0]
+
+
+@pytest.fixture
+def key(fernet_and_key):
+    return fernet_and_key[1]
+
+
+def test_invalid_key():
+    with pytest.raises(ValueError):
+        EncryptedCookieStorage(b'123')  # short key
+
+
+@asyncio.coroutine
+def test_create_new_sesssion_broken_by_format(test_client, fernet, key):
 
     @asyncio.coroutine
-    def create_server(self, method, path, handler):
-        middleware = session_middleware(
-            EncryptedCookieStorage(self.key))
-        app = web.Application(middlewares=[middleware], loop=self.loop)
-        app.router.add_route(method, path, handler)
+    def handler(request):
+        session = yield from get_session(request)
+        assert isinstance(session, Session)
+        assert session.new
+        assert not session._changed
+        assert {} == session
+        return web.Response(body=b'OK')
 
-        port = self.find_unused_port()
-        handler = app.make_handler()
-        srv = yield from self.loop.create_server(
-            handler, '127.0.0.1', port)
-        url = "http://127.0.0.1:{}".format(port) + path
-        self.srv = srv
-        self.handler = handler
-        return app, srv, url
+    new_fernet = Fernet(Fernet.generate_key())
+    client = yield from test_client(create_app, handler, key)
+    make_cookie(client, new_fernet, {'a': 1, 'b': 12})
+    resp = yield from client.get('/')
+    assert resp.status == 200
 
-    def make_cookie(self, data):
-        session_data = {
-            'session': data,
-            'created': int(time.time())
-        }
 
-        cookie_data = json.dumps(session_data).encode('utf-8')
-        data = self.fernet.encrypt(cookie_data).decode('utf-8')
+@asyncio.coroutine
+def test_load_existing_sesssion(test_client, fernet, key):
 
-        return {'AIOHTTP_SESSION': data}
+    @asyncio.coroutine
+    def handler(request):
+        session = yield from get_session(request)
+        assert isinstance(session, Session)
+        assert not session.new
+        assert not session._changed
+        assert {'a': 1, 'b': 12} == session
+        return web.Response(body=b'OK')
 
-    def decrypt(self, cookie_value):
-        assert type(cookie_value) == str
-        return json.loads(
-            self.fernet.decrypt(cookie_value.encode('utf-8')).decode('utf-8')
-        )
+    client = yield from test_client(create_app, handler, key)
+    make_cookie(client, fernet, {'a': 1, 'b': 12})
+    resp = yield from client.get('/')
+    assert resp.status == 200
 
-    def test_invalid_key(self):
-        with self.assertRaises(ValueError):
-            EncryptedCookieStorage(b'123')  # short key
 
-    def test_create_new_sesssion_broken_by_fermat(self):
+@asyncio.coroutine
+def test_change_sesssion(test_client, fernet, key):
 
-        @asyncio.coroutine
-        def handler(request):
-            session = yield from get_session(request)
-            self.assertIsInstance(session, Session)
-            self.assertTrue(session.new)
-            self.assertFalse(session._changed)
-            self.assertEqual({}, session)
-            return web.Response(body=b'OK')
+    @asyncio.coroutine
+    def handler(request):
+        session = yield from get_session(request)
+        session['c'] = 3
+        return web.Response(body=b'OK')
 
-        @asyncio.coroutine
-        def go():
-            key = fernet.Fernet.generate_key()
-            self.key = base64.urlsafe_b64decode(key)
+    client = yield from test_client(create_app, handler, key)
+    make_cookie(client, fernet, {'a': 1, 'b': 2})
+    resp = yield from client.get('/')
+    assert resp.status == 200
 
-            _, _, url = yield from self.create_server(
-                'GET', '/', handler)
-            resp = yield from request(
-                'GET', url,
-                cookies=self.make_cookie({'a': 1, 'b': 12}),
-                loop=self.loop)
-            self.assertEqual(200, resp.status)
+    morsel = resp.cookies['AIOHTTP_SESSION']
+    cookie_data = decrypt(fernet, morsel.value)
+    assert 'session' in cookie_data
+    assert 'a' in cookie_data['session']
+    assert 'b' in cookie_data['session']
+    assert 'c' in cookie_data['session']
+    assert 'created' in cookie_data
+    assert cookie_data['session']['a'] == 1
+    assert cookie_data['session']['b'] == 2
+    assert cookie_data['session']['c'] == 3
+    assert morsel['httponly']
+    assert '/' == morsel['path']
 
-        self.loop.run_until_complete(go())
 
-    def test_load_existing_sesssion(self):
+@asyncio.coroutine
+def test_clear_cookie_on_sesssion_invalidation(test_client, fernet, key):
 
-        @asyncio.coroutine
-        def handler(request):
-            session = yield from get_session(request)
-            self.assertIsInstance(session, Session)
-            self.assertFalse(session.new)
-            self.assertFalse(session._changed)
-            self.assertEqual({'a': 1, 'b': 12}, session)
-            return web.Response(body=b'OK')
+    @asyncio.coroutine
+    def handler(request):
+        session = yield from get_session(request)
+        session.invalidate()
+        return web.Response(body=b'OK')
 
-        @asyncio.coroutine
-        def go():
-            _, _, url = yield from self.create_server('GET', '/', handler)
-            resp = yield from request(
-                'GET', url,
-                cookies=self.make_cookie({'a': 1, 'b': 12}),
-                loop=self.loop)
-            self.assertEqual(200, resp.status)
+    client = yield from test_client(create_app, handler, key)
+    make_cookie(client, fernet, {'a': 1, 'b': 2})
+    resp = yield from client.get('/')
+    assert resp.status == 200
 
-        self.loop.run_until_complete(go())
-
-    def test_change_sesssion(self):
-
-        @asyncio.coroutine
-        def handler(request):
-            session = yield from get_session(request)
-            session['c'] = 3
-            return web.Response(body=b'OK')
-
-        @asyncio.coroutine
-        def go():
-            _, _, url = yield from self.create_server('GET', '/', handler)
-            resp = yield from request(
-                'GET', url,
-                cookies=self.make_cookie({'a': 1, 'b': 2}),
-                loop=self.loop)
-            self.assertEqual(200, resp.status)
-            morsel = resp.cookies['AIOHTTP_SESSION']
-            cookie_data = self.decrypt(morsel.value)
-            self.assertIn('session', cookie_data)
-            self.assertIn('a', cookie_data['session'])
-            self.assertIn('b', cookie_data['session'])
-            self.assertIn('c', cookie_data['session'])
-            self.assertIn('created', cookie_data)
-            self.assertEqual(cookie_data['session']['a'], 1)
-            self.assertEqual(cookie_data['session']['b'], 2)
-            self.assertEqual(cookie_data['session']['c'], 3)
-            self.assertTrue(morsel['httponly'])
-            self.assertEqual('/', morsel['path'])
-
-        self.loop.run_until_complete(go())
-
-    def test_clear_cookie_on_sesssion_invalidation(self):
-
-        @asyncio.coroutine
-        def handler(request):
-            session = yield from get_session(request)
-            session.invalidate()
-            return web.Response(body=b'OK')
-
-        @asyncio.coroutine
-        def go():
-            _, _, url = yield from self.create_server('GET', '/', handler)
-            resp = yield from request(
-                'GET', url,
-                cookies=self.make_cookie({'a': 1, 'b': 2}),
-                loop=self.loop)
-            self.assertEqual(200, resp.status)
-            morsel = resp.cookies['AIOHTTP_SESSION']
-            self.assertEqual('', morsel.value)
-            self.assertFalse(morsel['httponly'])
-            self.assertEqual(morsel['path'], '/')
-
-        self.loop.run_until_complete(go())
+    morsel = resp.cookies['AIOHTTP_SESSION']
+    assert '' == morsel.value
+    assert not morsel['httponly']
+    assert morsel['path'] == '/'

5fd58a2e822f019fe64333dacc3856118c3b418e
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-08-03 18:04:13 +0300

    Finish tests conversion

--- tests/test_http_exception.py
+++ tests/test_http_exception.py
@@ -1,77 +1,39 @@
 import asyncio
-import socket
-import unittest
 
-from aiohttp import web, request
+from aiohttp import web
 from aiohttp_session import (session_middleware,
                              get_session, SimpleCookieStorage)
 
 
-class TestHttpException(unittest.TestCase):
+def create_app(loop, *handlers):
+    middleware = session_middleware(SimpleCookieStorage())
+    app = web.Application(middlewares=[middleware], loop=loop)
+    for url, handler in handlers:
+        app.router.add_route('GET', url, handler)
+    return app
 
-    def setUp(self):
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(None)
-        self.srv = None
-        self.handler = None
 
-    def tearDown(self):
-        self.loop.run_until_complete(self.handler.finish_connections())
-        self.srv.close()
-        self.loop.stop()
-        self.loop.run_forever()
-        self.loop.close()
-
-    def find_unused_port(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('127.0.0.1', 0))
-        port = s.getsockname()[1]
-        s.close()
-        return port
+@asyncio.coroutine
+def test_exceptions(test_client):
 
     @asyncio.coroutine
-    def create_server(self, routes):
-        middleware = session_middleware(SimpleCookieStorage())
-        app = web.Application(middlewares=[middleware], loop=self.loop)
-        for method, path, handler in routes:
-            app.router.add_route(method, path, handler)
+    def save(request):
+        session = yield from get_session(request)
+        session['message'] = 'works'
+        raise web.HTTPFound('/show')
 
-        port = self.find_unused_port()
-        handler = app.make_handler()
-        srv = yield from self.loop.create_server(
-            handler, '127.0.0.1', port)
-        url = "http://127.0.0.1:{}".format(port)
-        self.handler = handler
-        self.srv = srv
-        return app, srv, url
+    @asyncio.coroutine
+    def show(request):
+        session = yield from get_session(request)
+        message = session.get('message')
+        return web.Response(text=str(message))
 
-    def test_exceptions(self):
+    client = yield from test_client(create_app,
+                                    ('/save', save),
+                                    ('/show', show))
 
-        @asyncio.coroutine
-        def save(request):
-            session = yield from get_session(request)
-            session['message'] = 'works'
-            raise web.HTTPFound('/show')
-
-        @asyncio.coroutine
-        def show(request):
-            session = yield from get_session(request)
-            message = session.get('message')
-            return web.Response(text=str(message))
-
-        def get_routes():
-            return [
-                ['GET', '/save', save],
-                ['GET', '/show', show],
-            ]
-
-        @asyncio.coroutine
-        def go():
-            _, _, url = yield from self.create_server(get_routes())
-            resp = yield from request('GET', url + '/save', loop=self.loop)
-            self.assertEqual(200, resp.status)
-            self.assertEqual(resp.url[-5:], '/show')
-            text = yield from resp.text()
-            assert text == 'works'
-
-        self.loop.run_until_complete(go())
+    resp = yield from client.get('/save')
+    assert resp.status == 200
+    assert resp.url[-5:] == '/show'
+    text = yield from resp.text()
+    assert text == 'works'

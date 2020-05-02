6f2d3872ac68ed9f6d403ec4657d55969a48cab7
Jeremy Vriens <jeremyvriens1993@gmail.com>
2020-05-02 23:44:21 +0200

    Use the on_startup signals and store the redis pool in the Application instance.

--- demo/redis_storage.py
+++ demo/redis_storage.py
@@ -1,9 +1,8 @@
-import asyncio
 import aioredis
 import time
 
 from aiohttp import web
-from aiohttp_session import setup, get_session
+from aiohttp_session import get_session, setup
 from aiohttp_session.redis_storage import RedisStorage
 
 
@@ -15,23 +14,26 @@ async def handler(request):
     return web.Response(text=text)
 
 
-async def make_redis_pool():
+async def create_redis(app):
     redis_address = ('127.0.0.1', '6379')
-    return await aioredis.create_redis_pool(redis_address, timeout=1)
+    app["redis"] = await aioredis.create_redis_pool(redis_address, timeout=1)
+
+
+async def dispose_redis(app):
+    app["redis"].close()
+    await app["redis"].wait_closed()
+
+
+async def setup_aiohttp_session(app):
+    storage = RedisStorage(app["redis"])
+    setup(app, storage)
 
 
 def make_app():
-    loop = asyncio.get_event_loop()
-    redis_pool = loop.run_until_complete(make_redis_pool())
-    storage = RedisStorage(redis_pool)
-
-    async def dispose_redis_pool(app):
-        redis_pool.close()
-        await redis_pool.wait_closed()
-
     app = web.Application()
-    setup(app, storage)
-    app.on_cleanup.append(dispose_redis_pool)
+    app.on_startup.append(create_redis)
+    app.on_startup.append(setup_aiohttp_session)
+    app.on_cleanup.append(dispose_redis)
     app.router.add_get('/', handler)
     return app
 

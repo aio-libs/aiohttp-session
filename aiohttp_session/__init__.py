efb5e196afbefe472391d4e1457c00a97444ca5e
Spencer Pearson <speeze.pearson+git@gmail.com>
2020-06-01 02:30:58 -0700

    docs

--- aiohttp_session/__init__.py
+++ aiohttp_session/__init__.py
@@ -255,7 +255,7 @@ class AbstractStorage(metaclass=abc.ABCMeta):
 _cookie_unsafe_char = re.compile(r'[% ",;\\]')
 def _to_cookiesafe_json(data) -> str:
     '''Turns a JSON-serializable value into a %-encoded string
-    suitable for use in a cookie.
+    suitable for use in a cookie, as described by RFC 6265.
     '''
     return _cookie_unsafe_char.sub(lambda m: '%'+hex(ord(m.group()))[2:], json.dumps(data))
 def _from_cookiesafe_json(cookie: str):

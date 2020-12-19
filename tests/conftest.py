4b33d56e3d0406ee65b55b7295e9bd1428c2a09c
Sam Bull <aa6bs0@sambull.org>
2020-12-19 14:01:36 +0000

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -76,14 +76,13 @@ def redis_server(docker, session_id, loop, request):
         host = inspection['NetworkSettings']['IPAddress']
         port = 6379
 
-    async def set_redis(host, port, loop):
-        conn = await aioredis.create_connection((host, port), loop=loop)
-        await conn.execute('SET', 'foo', 'bar')
-
     delay = 0.1
     for i in range(20):
         try:
-            loop.run_until_complete(set_redis(host, port, loop))
+            conn = loop.run_until_complete(
+                aioredis.create_connection((host, port), loop=loop)
+            )
+            loop.run_until_complete(conn.execute('SET', 'foo', 'bar'))
             break
         except ConnectionRefusedError:
             time.sleep(delay)

245a3e4b56557cd69784db767bced7f9c31e43fc
Panagiotis Kolokotronis <panos122008@gmail.com>
2021-03-21 21:04:15 +0200

    Remember to remove explicit type annotation w/ aiohttp 3.8

--- aiohttp_session/__init__.py
+++ aiohttp_session/__init__.py
@@ -279,6 +279,7 @@ class AbstractStorage(metaclass=abc.ABCMeta):
         pass
 
     def load_cookie(self, request: web.Request) -> Optional[str]:
+        # TODO: Remove explicit type anotation when aiohttp 3.8 is out
         cookie: Optional[str] = request.cookies.get(self._cookie_name)
         return cookie
 

925bd86673ab7d8f5d0a4fe7856d80c25d5685d3
Sam Bull <aa6bs0@sambull.org>
2021-04-25 16:14:13 +0100

    Update test_redis_storage.py

--- tests/test_redis_storage.py
+++ tests/test_redis_storage.py
@@ -342,8 +342,7 @@ async def test_redis_from_create_pool(
         pass
 
     redis = await aioredis.create_pool(**redis_params)
-    with pytest.warns(DeprecationWarning):
-        create_app(handler=handler, redis=redis)
+    create_app(handler=handler, redis=redis)
 
 
 async def test_not_redis_provided_to_storage() -> None:

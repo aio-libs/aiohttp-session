efa8301cdde7528a79c36a89f484a575b4d0f2ed
Sam Bull <aa6bs0@sambull.org>
2021-08-07 13:16:31 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,9 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
-import os
 import codecs
+import os
+import sys
 import re
 
 _docs_path = os.path.dirname(__file__)
